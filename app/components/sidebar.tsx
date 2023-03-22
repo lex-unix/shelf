acd982eb27b5be2342171aad958d927c4a6ab6ab
Lex Nousagi <alexmiyin@gmail.com>
2023-03-22 18:13:54 +0200

    feat(sidebar): show active state of sidebar

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
   CheckIcon
 } from '@heroicons/react/24/outline'
+import { Link, useSearchParams } from '@remix-run/react'
 import { type ReactNode } from 'react'
 
 export default function Sidebar() {
@@ -15,16 +16,16 @@ export default function Sidebar() {
           <SidebarItem>
             <BookOpenIcon className="h-6 w-6" /> <span>All books</span>
           </SidebarItem>
-          <SidebarItem>
+          <SidebarItem tag="favorite">
             <StarIcon className="h-6 w-6" /> <span>Favorites</span>
           </SidebarItem>
-          <SidebarItem>
+          <SidebarItem tag="currentlyReading">
             <BookmarkIcon className="h-6 w-6" /> <span>Currently reading</span>
           </SidebarItem>
-          <SidebarItem>
+          <SidebarItem tag="wantToRead">
             <InboxIcon className="h-6 w-6" /> <span>Want to read</span>
           </SidebarItem>
-          <SidebarItem>
+          <SidebarItem tag="finished">
             <CheckIcon className="h-6 w-6" /> <span>Finished</span>
           </SidebarItem>
         </div>
@@ -34,13 +35,23 @@ export default function Sidebar() {
 }
 
 interface SidebarItemProps {
+  tag?: string
   children: ReactNode
 }
 
-function SidebarItem({ children }: SidebarItemProps) {
+function SidebarItem({ tag, children }: SidebarItemProps) {
+  const href = tag ? `/library?tag=${tag}` : '/library'
+  const [params] = useSearchParams()
+  const isActive = tag === params.get('tag') || (!tag && !params.get('tag'))
+
   return (
-    <div className="flex w-full items-center justify-start gap-3 rounded-lg px-3 py-2 hover:bg-white/5">
+    <Link
+      to={href}
+      className={`${
+        isActive ? 'bg-white/5' : 'bg-transparent'
+      } flex w-full items-center justify-start gap-3 rounded-lg px-3 py-2 transition-colors hover:bg-white/5`}
+    >
       {children}
-    </div>
+    </Link>
   )
 }
