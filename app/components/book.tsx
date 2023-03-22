15b6580c5c35a19af1298a0d9354fea3c677cf9e
Lex Nousagi <alexmiyin@gmail.com>
2023-03-22 14:26:46 +0200

    feat(book): add component

---
+++ app/components/book.tsx
@@ -0,0 +1,17 @@
+interface BookProps {
+  author: string
+  book: string
+}
+export default function Book({ author, book }: BookProps) {
+  return (
+    <div className="aspect-[2/3] w-[150px] rounded border border-neutral-700 bg-neutral-800 shadow-[inset_6px_-4px_12px_rgba(0,0,0,0.1)]">
+      <div className="flex h-full justify-between px-2.5 font-medium">
+        <div className="h-full w-[1px] bg-neutral-700"></div>
+        <div className="flex h-full flex-1 flex-col items-center justify-between py-8">
+          <p>{book}</p>
+          <p className="text-neutral-400">{author}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
