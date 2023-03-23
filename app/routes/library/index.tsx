8383190d57e86cbf0c099a5d90186136f1e2d801
Lex Nousagi <alexmiyin@gmail.com>
2023-03-23 13:55:09 +0200

    refactor(library): filter books by tag on the server

--- app/routes/library/index.tsx
+++ app/routes/library/index.tsx
@@ -1,4 +1,5 @@
-import { useLoaderData, useSearchParams } from '@remix-run/react'
+import { type LoaderArgs } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
 import { type ChangeEvent, useState, useMemo } from 'react'
 import LibraryViewBar from '~/components/library-view-bar'
 import ListView from '~/components/list-view'
@@ -6,32 +7,31 @@ import Sidebar from '~/components/sidebar'
 import TileView from '~/components/tile-view'
 import { books } from '~/fixtures/book'
 
-export const loader = async () => {
-  return books
+export const loader = async ({ request }: LoaderArgs) => {
+  const url = new URL(request.url)
+  const tag = url.searchParams.get('tag')
+  let selecetedBooks = books
+  if (tag) {
+    selecetedBooks = selecetedBooks.filter(book => book.tag === tag)
+  }
+  return selecetedBooks
 }
 
 export default function LibraryIndexPage() {
   const books = useLoaderData<typeof loader>()
-  const [searchParams] = useSearchParams()
   const [view, setView] = useState<'tile' | 'list'>('tile')
   const [search, setSearch] = useState('')
 
-  const tag = searchParams.get('tag')
+  const filteredBooks = useMemo(
+    () =>
+      books.filter(
+        b =>
+          b.author.toLowerCase().includes(search.toLowerCase()) ||
+          b.book.toLowerCase().includes(search.toLowerCase())
+      ),
 
-  const filteredBooks = useMemo(() => {
-    let filteredBooks = books.filter(
-      b =>
-        b.author.toLowerCase().includes(search.toLowerCase()) ||
-        b.book.toLowerCase().includes(search.toLowerCase())
-    )
-    if (tag) {
-      filteredBooks = filteredBooks.filter(b =>
-        b.tag.toLowerCase().includes(tag.toLowerCase())
-      )
-    }
-
-    return filteredBooks
-  }, [books, search, tag])
+    [books, search]
+  )
 
   return (
     <div className="mt-8 flex">
