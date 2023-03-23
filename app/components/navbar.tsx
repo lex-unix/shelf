ace033bc0890084026e5ba379d0c8da3e7bcdd60
Lex Nousagi <alexmiyin@gmail.com>
2023-03-23 11:05:49 +0200

    feat(navbar): keyboard navigation

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,6 +1,30 @@
+import { useNavigate } from '@remix-run/react'
+import { useEffect } from 'react'
 import Avatar from './avatar'
 
 export default function Navbar() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const keyDown = (e: KeyboardEvent) => {
+      if (e.shiftKey && e.key === 'L') {
+        return navigate('/library')
+      }
+
+      if (e.shiftKey && e.key === 'G') {
+        return navigate('/goals')
+      }
+
+      if (e.shiftKey && e.key === 'P') {
+        return navigate('/preferences')
+      }
+    }
+
+    document.addEventListener('keydown', keyDown)
+
+    return () => document.removeEventListener('keydown', keyDown)
+  }, [navigate])
+
   return (
     <div className="borde-b mt-5 h-10 w-full">
       <div className="flex h-full max-w-5xl items-center justify-between">
