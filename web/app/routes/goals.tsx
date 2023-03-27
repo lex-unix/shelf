3399653175fb3c88e3b6ded00318981f467ba0d7
Lex Nousagi <alexmiyin@gmail.com>
2023-03-27 14:45:51 +0300

    feat(framer-motion): add

--- web/app/routes/goals.tsx
+++ web/app/routes/goals.tsx
@@ -1,19 +1,40 @@
 import { PlusIcon } from '@heroicons/react/24/outline'
-import { type ActionFunction } from '@remix-run/node'
+import { type LoaderArgs, type ActionFunction } from '@remix-run/node'
+import { useFetcher, useLoaderData } from '@remix-run/react'
 import Button from '~/components/button'
 import CircleProgress from '~/components/circle-progress'
 import GoalForm from '~/components/goal-form'
 import Popover from '~/components/popover'
+import { AnimatePresence, motion } from 'framer-motion'
+
+const API = 'http://127.0.0.1:3001/api'
+
+type Goal = {
+  id: number
+  name: string
+}
+
+export const loader = async ({ request }: LoaderArgs) => {
+  const res = await fetch(API + '/goals', {
+    credentials: 'include'
+  })
+  const goals = (await res.json()) as { goals: Goal[] }
+  return goals
+}
 
 export const action: ActionFunction = async ({ request }) => {
   const form = await request.formData()
   const data = Object.fromEntries(form)
-  console.log(data)
-  await new Promise(resolve => setTimeout(resolve, 2000))
   return data
 }
 
+const ids = new Array(2).fill(0)
+
 export default function GoalsPage() {
+  const loaderData = useLoaderData<typeof loader>()
+  const fetcher = useFetcher()
+  console.log(loaderData)
+
   return (
     <div className="mx-auto max-w-2xl py-6">
       <div className="mb-6 flex items-center justify-between">
@@ -27,43 +48,66 @@ export default function GoalsPage() {
           </Popover.Content>
         </Popover>
       </div>
-      <ul className="space-y-4">
-        <li className="flex items-start rounded-md border border-gray-700 p-6">
-          <CircleProgress progress={90} currentCount={9} />
+      <ul className="">
+        <AnimatePresence initial={false}>
+          {ids.map((_, id) => (
+            <motion.li
+              key={id}
+              initial={{ opacity: 0, height: 0 }}
+              animate={{ opacity: 1, height: 'auto' }}
+              exit={{ opacity: 0, height: 0 }}
+              transition={{ opacity: { duration: 0.2 } }}
+              className="rounded-md border border-gray-700"
+            >
+              <fetcher.Form
+                method="post"
+                replace
+                className="flex items-start p-6"
+              >
+                <input type="hidden" name="id" value={id} />
+                <CircleProgress progress={90} currentCount={9} />
+                <div className="flex flex-1 items-start justify-between">
+                  <div>
+                    <p className="text-gray-400">Goal {id + 1}</p>
+                    <p className="mt-1">2023 Reading goals</p>
+                  </div>
+                  <button className="text-xl">&times;</button>
+                </div>
+              </fetcher.Form>
+            </motion.li>
+          ))}
+        </AnimatePresence>
+      </ul>
+    </div>
+  )
+}
+
+/*
+function GoalItem({ id }: { id: number }) {
+  const fetcher = useFetcher()
+  const isDeleting = fetcher.submission?.formData.get('id') === id.toString()
+
+  if (isDeleting) return null
+
+  return (
+    <motion.li
+      initial={{ opacity: 0, height: 0 }}
+      animate={{ opacity: 1, height: 'auto' }}
+      exit={{ opacity: 0, height: 0 }}
+      className="rounded-md border border-gray-700"
+    >
+      <fetcher.Form method="post" replace className="flex items-start p-6">
+        <input type="hidden" name="id" value={id} />
+        <CircleProgress progress={90} currentCount={9} />
+        <div className="flex flex-1 items-start justify-between">
           <div>
             <p className="text-gray-400">Goal</p>
             <p className="mt-1">2023 Reading goals</p>
           </div>
-        </li>
-        <li className="flex items-start rounded-md border border-gray-700 p-6">
-          <CircleProgress progress={75} currentCount={12} />
-          <div>
-            <p className="text-gray-400">Goal</p>
-            <p className="mt-1">Programming</p>
-          </div>
-        </li>
-      </ul>
-      <div className="mt-6">
-        <div className="mb-6 flex items-center justify-between">
-          <h1 className="text-xl font-semibold">Past goals</h1>
+          <button className="text-xl">&times;</button>
         </div>
-        <ul className="space-y-4">
-          <li className="flex items-start rounded-md border border-gray-700 p-6">
-            <CircleProgress progress={100} currentCount={3} />
-            <div>
-              <p className="text-gray-400">Goal</p>
-              <p className="mt-1">2022 reading goals</p>
-            </div>
-          </li>
-          <li className="flex items-start rounded-md border border-gray-700 p-6">
-            <CircleProgress progress={100} currentCount={20} />
-            <div>
-              <p className="text-gray-400">Goal</p>
-              <p className="mt-1">Ukrainian literature</p>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
+      </fetcher.Form>
+    </motion.li>
   )
 }
+*/
