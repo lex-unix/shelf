b01323c20cb2fc99db0806b1d9d8734a61a2a133
Lex Nousagi <alexmiyin@gmail.com>
2023-04-08 13:59:14 +0300

    chore(web): support vercel deployment

--- web/remix.config.js
+++ web/remix.config.js
@@ -3,7 +3,9 @@ module.exports = {
   ignoredRouteFiles: ['**/.*'],
   future: {
     unstable_tailwind: true
-  }
+  },
+  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
+  serverBuildPath: 'api/index.js'
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
