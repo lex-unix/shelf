1c1e5f641e5275ad8cf41e6155d1cc9d6447ec0a
Lex Nousagi <alexmiyin@gmail.com>
2023-05-21 11:44:25 +0300

    fix(index): correct route path

--- web/app/routes/_index.tsx
+++ web/app/routes/_index.tsx
@@ -4,7 +4,7 @@ export default function Index() {
   return (
     <div className="mt-10">
       <h1 className="text-4xl">Hello, Remix! 💿</h1>
-      <Link to="/library" className="font-bold">
+      <Link to="/library/books" className="font-bold">
         Library
       </Link>
     </div>
