de4de6b1f147573a65cc40c33926215681cbfcdc
Lex Nousagi <alexmiyin@gmail.com>
2023-05-21 12:13:06 +0300

    chore(remix): update remix

--- web/remix.config.js
+++ web/remix.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
   future: {
-    unstable_tailwind: true,
     v2_errorBoundary: true,
     v2_routeConvention: true,
     v2_normalizeFormMethod: true
-  }
+  },
+  tailwind: true
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
