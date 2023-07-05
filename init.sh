7ec5d9dac72124a2dc39ff2d2ac531b035a76f12
Lex Nousagi <alexmiyin@gmail.com>
2023-07-05 18:41:10 +0300

    chore(init.sh): formatting

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
 sudo apt-get update
@@ -9,6 +9,6 @@ sudo chmod a+r /etc/apt/keyrings/docker.gnupg
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
