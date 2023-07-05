f034e2cf5878dda70a2f4ec55cfa14cb1d082bdc
Lex Nousagi <alexmiyin@gmail.com>
2023-07-05 16:47:14 +0300

    refactor(server): run prettier

--- server/index.ts
+++ server/index.ts
@@ -1,23 +1,23 @@
-import * as dotenv from "dotenv";
-import fastify from "fastify";
-import { getConfig } from "./lib/config/config";
-import startServer from "./lib/server";
+import * as dotenv from 'dotenv'
+import fastify from 'fastify'
+import { getConfig } from './lib/config/config'
+import startServer from './lib/server'
 
-dotenv.config();
+dotenv.config()
 
 const main = async () => {
-  process.on("unhandledRejection", (err) => {
-    console.error(err);
-    process.exit(1);
-  });
+  process.on('unhandledRejection', err => {
+    console.error(err)
+    process.exit(1)
+  })
 
-  const config = getConfig();
+  const config = getConfig()
 
-  const server = fastify(config.server);
-  server.register(startServer, config);
+  const server = fastify(config.server)
+  server.register(startServer, config)
 
-  const address = await server.listen(config.listen);
-  server.log.info(`Server running at: ${address}`);
-};
+  const address = await server.listen(config.listen)
+  server.log.info(`Server running at: ${address}`)
+}
 
-main();
+main()
