3ca3267069fcb50f20fa03b20994152c3b5f1684
Lex Nousagi <alexmiyin@gmail.com>
2023-07-08 20:03:34 +0300

    chore(warning-banner): update styles

--- web/app/components/warning-banner.tsx
+++ web/app/components/warning-banner.tsx
@@ -2,8 +2,8 @@ import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
 
 export default function WarningBanner() {
   return (
-    <div className="w-full rounded border border-yellow-700 bg-yellow-900">
-      <div className="flex items-start py-2 px-3 text-yellow-300">
+    <div className="w-full rounded border border-yellow-700 bg-[#241D0B]">
+      <div className="flex items-start px-3 py-2 text-yellow-300">
         <div className="mt-1.5">
           <ExclamationTriangleIcon className="h-6 w-6" />
         </div>
@@ -11,7 +11,7 @@ export default function WarningBanner() {
           <h2 className="inline-block text-xl font-semibold">
             This section is under construction
           </h2>
-          <p className="mt-2">
+          <p className="mt-2 opacity-70">
             This feature will be available in the following major release. Stay
             tuned.
           </p>
