21dec5e9ae706c6040b8f4c1cc259679cdbaacf7
Lex Nousagi <alexmiyin@gmail.com>
2023-08-30 20:39:26 +0300

    refactor(models): use template string for sql queries

--- server/lib/models/users.ts
+++ server/lib/models/users.ts
@@ -8,8 +8,7 @@ interface UserBase {
 export default function usersModel(db: Pool) {
   return {
     registerUser: async function (user: UserBase, hash: string) {
-      const sql =
-        'INSERT INTO Account (name, password, email) VALUES ($1, $2, $3) RETURNING id'
+      const sql = `INSERT INTO Account (name, password, email) VALUES ($1, $2, $3) RETURNING id`
       const values = [user.name, hash, user.email]
       const result = await db.query(sql, values)
       const { id } = result.rows[0] as { id: number }
@@ -17,14 +16,13 @@ export default function usersModel(db: Pool) {
     },
 
     getUserById: async function (id: number) {
-      const sql = 'SELECT id, name, email, password FROM Account WHERE id = $1'
+      const sql = `SELECT id, name, email, password FROM Account WHERE id = $1`
       const result = await db.query(sql, [id])
       return result.rows[0]
     },
 
     getUserByEmail: async function (email: string) {
-      const sql =
-        'SELECT id, name, email, password FROM Account WHERE email = $1'
+      const sql = `SELECT id, name, email, password FROM Account WHERE email = $1`
       const result = await db.query(sql, [email])
       return result.rows[0]
     },
