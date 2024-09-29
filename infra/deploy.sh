ac7b17eb48ab4b8ab14bf50f92b574960c0abed0
Lex Miin <alexmiyin@gmail.com>
2024-09-29 13:13:48 +0300

    chore: simplify database setup in deployment script
    -  Remove direct SQL file copying from deploy script
    -  Update Docker Compose to use local SQL files

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -16,17 +16,10 @@ rsync -avzP -e ssh ./docker-compose.yaml $SSH_USER@$SSH_HOST:$WORK_DIR
 
 ssh $SSH_USER@$SSH_HOST "cd $WORK_DIR && docker compose up -d"
 
-ssh $SSH_USER@$SSH_HOST "cd $WORK_DIR && docker compose cp create-tables.sql db:/tmp/create-tables.sql"
-ssh $SSH_USER@$SSH_HOST "cd $WORK_DIR && docker compose cp create-triggers.sql db:/tmp/create-triggers.sql"
-
-ssh $SSH_USER@$SSH_HOST "cd $WORK_DIR && docker compose exec db psql -U lex -d shelf -f /tmp/create-tables.sql"
-ssh $SSH_USER@$SSH_HOST "cd $WORK_DIR && docker compose exec db psql -U lex -d shelf -f /tmp/create-triggers.sql"
-
 ssh-keygen -t rsa -b 4096 -f ./ssh_key
 ssh-copy-id -i ./ssh_key.pub $SSH_USER@$SSH_HOST
 
 SSH_PRIVATE_KEY=$(cat ./ssh_key)
-
 
 gh secret set SSH_PRIVATE_KEY --body "$SSH_PRIVATE_KEY"
 gh secret set SSH_HOST --body "$SSH_HOST"
