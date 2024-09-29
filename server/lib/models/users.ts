c0f0fdf1294edb65e7ad4fa16412e75bb32ba7e2
Lex Miin <alexmiyin@gmail.com>
2024-09-29 13:12:39 +0300

    chore: bump server deps

--- server/lib/models/users.ts
+++ server/lib/models/users.ts
@@ -7,7 +7,7 @@ interface UserBase {
 
 export default function usersModel(db: Pool) {
   return {
-    registerUser: async function (user: UserBase, hash: string) {
+    registerUser: async function(user: UserBase, hash: string) {
       const sql = `INSERT INTO Account (name, password, email) VALUES ($1, $2, $3) RETURNING id`
       const values = [user.name, hash, user.email]
       const result = await db.query(sql, values)
@@ -15,24 +15,24 @@ export default function usersModel(db: Pool) {
       return await this.getUserById(id)
     },
 
-    getUserById: async function (id: number) {
+    getUserById: async function(id: number) {
       const sql = `SELECT id, name, email, password FROM Account WHERE id = $1`
       const result = await db.query(sql, [id])
       return result.rows[0]
     },
 
-    getUserByEmail: async function (email: string) {
+    getUserByEmail: async function(email: string) {
       const sql = `SELECT id, name, email, password FROM Account WHERE email = $1`
       const result = await db.query(sql, [email])
       return result.rows[0]
     },
 
-    updateUser: async function (user: UserBase, id: number) {
+    updateUser: async function(user: UserBase, id: number) {
       const sql = `UPDATE Account SET name = $1, email = $2 WHERE id = $3`
       await db.query(sql, [user.name, user.email, id])
     },
 
-    updatePassword: async function (hash: string, id: number) {
+    updatePassword: async function(hash: string, id: number) {
       const sql = `UPDATE Account SET password = $1 WHERE id = $2`
       await db.query(sql, [hash, id])
     }
